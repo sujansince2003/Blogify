fb8acea7c0b327524b2db034f7757f44967e64e4
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-03-03 02:07:30 +0545

    defined route handler to get ,update,create and delete blogs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,27 @@
 import { Hono } from "hono";
 import { getPrisma } from "../lib/prisma";
-import { decode, jwt, sign, verify } from "hono/jwt";
+import { sign, verify } from "hono/jwt";
 import { z } from "zod";
 import { createMiddleware } from "hono/factory";
 import bcrypt from "bcryptjs";
+
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+  Variables: {
+    userID: string;
+  };
+}>();
+
 const registerSchema = z.object({
   username: z.string().min(3, "Username must be at least 3 characters"),
-
   password: z.string().min(6, "password must be at least 6 character long"),
   email: z.string().email("email format didnot match"),
 });
 
-export const loginSchema = z
+const loginSchema = z
   .object({
     password: z.string().min(6, "Password must be at least 6 characters"),
   })
@@ -24,20 +34,7 @@ export const loginSchema = z
     ])
   );
 
-const app = new Hono<{
-  Bindings: {
-    DATABASE_URL: string;
-    JWT_SECRET: string;
-  }
-  Variables:
-  {
-    userID: string
-  }
-
-
-}>();
-
-//defining middleware
+//defining authmiddleware
 
 const authMiddleware = createMiddleware(async (c, next) => {
   try {
@@ -63,10 +60,23 @@ const authMiddleware = createMiddleware(async (c, next) => {
   }
 });
 
-app.get("/", async (c) => {
-  c.text("hello")
-})
-app.post("/api/v1/user/signup", async (c) => {
+app.get("/", authMiddleware, async (c) => {
+  const prisma = getPrisma(c.env.DATABASE_URL);
+  const user = await prisma.user.findFirst({
+    where: {
+      id: c.get("userID"),
+    },
+    include: {
+      blogs: true,
+    },
+  });
+  if (!user) {
+    return c.json({ msg: "user not found!!!" });
+  }
+
+  return c.json({ msg: "User found", user }, 200);
+});
+app.post("/api/v1/user/signup", authMiddleware, async (c) => {
   const { email, username, password } = await c.req.json();
 
   const validateInput = registerSchema.safeParse({ email, username, password });
@@ -131,17 +141,101 @@ app.post("/api/v1/user/signin", async (c) => {
   const token = await sign({ id: user.id }, c.env.JWT_SECRET);
   return c.json({ msg: "login successful", token }, 200);
 });
-app.post("/api/v1/blog", (c) => {
-  return c.text("Hello Hono!");
+
+const blogBodySchema = z.object({
+  title: z.string().min(1),
+  content: z.string(),
+  isPublished: z.boolean().optional(),
 });
-app.put("/api/v1/blog", (c) => {
-  return c.text("Hello Hono!");
+
+//post the blog
+app.post("/api/v1/blog", authMiddleware, async (c) => {
+  const blogBody = await c.req.json();
+  const isValid = blogBodySchema.safeParse(blogBody);
+  if (!isValid.success) {
+    return c.json({ msg: "Error in input validation" });
+  }
+  const blog = isValid.data;
+
+  const prisma = getPrisma(c.env.DATABASE_URL);
+  const createBlog = await prisma.blog.create({
+    data: {
+      title: blog.title,
+      content: blog.content,
+      userId: c.get("userID"),
+    },
+  });
+
+  return c.json({ msg: "blog created", createBlog });
 });
-app.get("/api/v1/blog/:id", (c) => {
-  return c.text("Hello Hono!");
+app.put("/api/v1/blog", authMiddleware, async (c) => {
+  const { title, content, id } = await c.req.json();
+  const prisma = getPrisma(c.env.DATABASE_URL);
+  const blogExist = await prisma.blog.findFirst({
+    where: {
+      id,
+    },
+  });
+  if (!blogExist) {
+    c.json({ msg: "blog dont exist" });
+  }
+
+  if (blogExist?.userId !== c.get("userID")) {
+    return c.json({ msg: "you cannot update this blog" }, 403);
+  }
+
+  const updatedBlog = await prisma.blog.update({
+    where: {
+      id,
+    },
+    data: {
+      title: title ?? blogExist?.title,
+      content: content ?? blogExist?.content,
+      updatedAt: new Date(),
+    },
+  });
+  return c.json({ msg: "updated successfully", updatedBlog }, 200);
 });
-app.get("/api/v1/blog/bulk", (c) => {
-  return c.text("Hello Hono!");
+app.delete("/api/v1/blog", authMiddleware, async (c) => {
+  const { title, content, id } = await c.req.json();
+  const prisma = getPrisma(c.env.DATABASE_URL);
+  const blogExist = await prisma.blog.findFirst({
+    where: {
+      id,
+    },
+  });
+  if (!blogExist) {
+    c.json({ msg: "blog dont exist" });
+  }
+
+  if (blogExist?.userId !== c.get("userID")) {
+    return c.json({ msg: "you cannot delete this blog" }, 403);
+  }
+
+  await prisma.blog.delete({
+    where: {
+      id,
+    },
+  });
+  return c.json({ msg: "delete successfully" }, 200);
+});
+app.get("/api/v1/blog/:id", authMiddleware, async (c) => {
+  const blogId = c.req.param("id");
+  const prisma = getPrisma(c.env.DATABASE_URL);
+
+  const blogExist = await prisma.blog.findUnique(blogId);
+  if (!blogExist) {
+    return c.json({ msg: "blog dont exist" });
+  }
+  if (blogExist.userId !== c.get("userID")) {
+    return c.json({ msg: "you are not allowed" });
+  }
+  return c.json({ msg: "successfully fetched", blogExist });
+});
+app.get("/api/v1/blog/bulk", async (c) => {
+  const prisma = getPrisma(c.env.DATABASE_URL);
+  const blogs = await prisma.blog.findMany();
+  return c.json({ msg: "fetched all blogs", blogs });
 });
 
 export default app;
