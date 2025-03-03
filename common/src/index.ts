f3c1b057c31ce693cae854ea02b90b13e568872d
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-03-03 09:33:10 +0545

    built a common module for using the zod schema for backend and types in frontend, published package to npm

---
+++ common/src/index.ts
@@ -0,0 +1,28 @@
+import { z } from "zod"
+export const registerSchema = z.object({
+    username: z.string().min(3, "Username must be at least 3 characters"),
+    password: z.string().min(6, "password must be at least 6 character long"),
+    email: z.string().email("email format didnot match"),
+});
+
+export const loginSchema = z
+    .object({
+        password: z.string().min(6, "Password must be at least 6 characters"),
+    })
+    .and(
+        z.union([
+            z.object({ email: z.string().email("Invalid email") }), // Login with email
+            z.object({
+                username: z.string().min(3, "Username must be at least 3 characters"),
+            }), // Login with username
+        ])
+    );
+export const blogBodySchema = z.object({
+    title: z.string().min(1),
+    content: z.string(),
+    isPublished: z.boolean().optional(),
+});
+
+export type signupType = z.infer<typeof registerSchema>
+export type loginType = z.infer<typeof loginSchema>
+export type blogType = z.infer<typeof blogBodySchema>
