3dad1d5839269f0f4c64ee49722933acea3a31bb
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-03-10 17:45:21 +0545

    updated the routes using contexts, protecting routes,not-found page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,41 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Blog, Login, SignUp, Blogs } from "./pages";
+import {
+  Blog,
+  Login,
+  SignUp,
+  Blogs,
+  Bookmarks,
+  WriteBlog,
+  Profile,
+  NotFound,
+} from "./pages";
+import Upload from "./pages/Upload";
+import { UserProvider } from "./context/UserContext";
+import { ProtectedRoute, AuthRoute } from "./components";
+
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/blogs" element={<Blogs />} />
-        </Routes>
-      </BrowserRouter>
+      <UserProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Blogs />} />
+            <Route element={<AuthRoute />}>
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<SignUp />} />
+            </Route>
+            <Route path="/blog/:xd" element={<Blog />} />
+            <Route path="/blogs" element={<Blogs />} />
+            <Route path="/upload" element={<Upload />} />
+            <Route element={<ProtectedRoute />}>
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/write" element={<WriteBlog />} />
+              <Route path="/bookmarks" element={<Bookmarks />} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </UserProvider>
     </>
   );
 }
