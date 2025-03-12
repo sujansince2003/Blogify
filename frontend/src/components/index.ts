2884bdcc744c632455dcc1ebdc6aed2b086a5bb0
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-03-12 23:55:20 +0545

    barrel file for comonents and UI pages

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,5 +1,9 @@
 import AuthForm from "./AuthForm"
 import AuthQuote from "./AuthQuote"
+import BlogCard from "./BlogCard"
+import Loading from "./Loading"
+import Navbar from "./Navbar"
+import AuthRoute from "./ProtectedRoutes/AuthRoute"
+import ProtectedRoute from "./ProtectedRoutes/ProtectedRoutes"
 
-
-export { AuthQuote, AuthForm }
+export { AuthQuote, AuthForm, BlogCard, Navbar, AuthRoute, ProtectedRoute, Loading }
--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,8 +1,15 @@
 import Blog from "./Blog";
 import Blogs from "./Blogs";
+import Bookmarks from "./Bookmarks";
 import Login from "./Login";
+import NotFound from "./NotFound";
+import Profile from "./Profile";
 import SignUp from "./Signup";
+import WriteBlog from "./WriteBlog";
 
 
 
-export { Blog, Login, SignUp, Blogs }
+
+
+
+export { Blog, Login, SignUp, Blogs, Bookmarks, WriteBlog, Profile, NotFound }
